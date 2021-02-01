73a54ff4d5141a52975a7cb0f6957981cabbdee0
Yves <yves.j@outlook.de>
2021-02-01 11:46:39 +0100

    _

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 javac GaussianAlgorithm.java
-java GaussianAlgorithm
 jar cfve release.jar GaussianAlgorithm GaussianAlgorithm.class GaussianAlignmentBuilder.class GaussianSolvedItem.class GaussianSolvedItemList.class GaussianSystem.class GaussianSystemInput.class GaussianUtilities.class GaussianAlignment.class GaussianLine.class
+java -jar release.jar
